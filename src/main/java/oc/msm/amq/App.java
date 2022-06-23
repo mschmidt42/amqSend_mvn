1a066c497b09b1c9b4636002cda617633c358e42
msm <michael.schmidt@opitz-consulting.com>
2022-06-23 14:27:21 +0200

    create maven project

---
+++ src/main/java/oc/msm/amq/App.java
@@ -0,0 +1,13 @@
+package oc.msm.amq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
